69a84638ac17e3dd2c487278cd1c2ef4b933f2b9
markusmannheim <markus@mannheim.media>
2021-02-28 22:46:05 +1100

    update

--- election.py
+++ election.py
@@ -1,182 +1,106 @@
 import pandas as pd
 import time
 
-def assign_votes():
-    for i, indice in enumerate(voters.index):
-        candidate = voters.loc[indice].votes[voters.loc[indice].pref]
-        value = voters.loc[indice].value
-        previous = candidates.loc[candidate, "votes"]
-        candidates.loc[candidate, "votes"] = previous + 1
-    candidates.sort_values("votes", ascending=False, inplace=True)
-    record_primary_votes()
-    check_elected()
+# functions
+def get_electorate():
+    ''' select electorate from text menu '''       
+    valid_response = True
+    while valid_response:
+        try:
+            print("\nWhich electorate do you wish to analyse?")
+            for i, electorate in enumerate(electorates.electorate):
+                print(f"{i + 1}. {electorate}")    
+            print("6. quit program")
+            text = input("Enter a number from 1 to 5:")            
+            text = int(text) - 1
+            valid_response = True
+            return text
+        except:
+            print("That's not a valid choice.")
 
-def record_primary_votes():
-    total_votes = candidates.votes.sum()
-    for candidate in candidates.index:
-        candidates.loc[candidate, "primary"] = f"{candidates.loc[candidate].votes / total_votes:.1%}"
-
-def check_elected():
-    global count_no
-    print(f"\n***** COUNT {count_no} *****")
-    reached_quota = candidates[candidates["votes"] >= quota]
-    if len(reached_quota) == 0:
-        print("no candidates elected")
-        count_no = count_no + 1
-        # select eliminated candidate
-        candidate = candidates.index[len(candidates) - 1]
-        name = candidates.loc[candidate].cname
-        party = candidates.loc[candidate].party
-        primary = candidates.loc[candidate].primary
-        print(f"{name} is eliminated!")
-        eliminated_candidates.loc[candidate] = [name, party, primary]
-        eliminate_candidate(candidate)
-    else:
-        candidate = reached_quota.index[0]
-        name = candidates.loc[candidate].cname
-        party = candidates.loc[candidate].party
-        primary = candidates.loc[candidate].primary
-        print(f"{name} is elected!")
-        elected_candidates.loc[candidate] = [name, party, primary]
-        if len(elected_candidates) < 5:
-            count_no = count_no + 1
-            redistribute_excess_votes(candidate)
-        else:
-            print("\nELECTED")
-            print(elected_candidates)
-            for candidate in candidates.index:
-                name = candidates.loc[candidate].cname
-                party = candidates.loc[candidate].party
-                primary = candidates.loc[candidate].primary
-                eliminated_candidates.loc[candidate] = [name, party, primary]
-            print("\nELIMINATED")
-            print(eliminated_candidates)
-            print(f"\nexhausted votes: {exhausted_votes:,.1f}")            
-
-def redistribute_excess_votes(candidate):    
-    votes = candidates.loc[candidate].votes
-    fraction = votes / quota - 1
-    name = candidates.loc[candidate].cname
-    print(f"redistributing {name}'s {votes:.0f} votes at a fractional value of {fraction:.3f} ...")
-    reallocate_votes(candidate, fraction)
-    check_elected()
-
-def eliminate_candidate(candidate):
-    votes = candidates.loc[candidate].votes
-    name = candidates.loc[candidate].cname
-    print(f"redistributing {name}'s {votes:.1f} votes ...")
-    reallocate_votes(candidate, 1)
-    check_elected()
-
-def reallocate_votes(candidate, fraction):
-    global candidates, exhausted_votes, voters
-    # iterrate through votes
-    for i, indice in enumerate(voters.index):
-        pref = voters.loc[indice].pref
-        # find votes for elected candidate
-        if voters.loc[indice].votes[pref] == candidate:
-            value = voters.loc[indice].value
-            while True:
-                pref = pref + 1                
-                voters.loc[indice].pref = pref                
-                # is it possible to distribute the vote?
-                if pref > len(voters.loc[indice].votes) - 1:
-                    exhausted_votes = exhausted_votes + value * fraction                    
-                    voters = voters[voters.index != indice]
-                    break
-                else:
-                    new_candidate = voters.loc[indice].votes[pref]
-                    if (new_candidate in eliminated_candidates.index) or (new_candidate in elected_candidates.index):
-                        pass                    
-                    else:                    
-                        candidates.loc[new_candidate, "votes"] = candidates.loc[new_candidate].votes + value * fraction
-                        break
-    print(f"exhausted votes: {exhausted_votes:,.1f}")
-    candidates = candidates[candidates.index != candidate]
-    candidates.sort_values("votes", ascending=False, inplace=True)
-
-def create_ids(df):
+def create_ids(label, df):
+    ''' creates unique IDs and sets them as the dataframe index '''
     start_time = time.time()
-    time_interval = 10
+    time_interval = 5
     ids = []
     for i, indice in enumerate(df.index):
         ids.append(f"{df.loc[indice].pcode}-{df.loc[indice].ccode}")
         if (time.time() - start_time) > time_interval:
-            print(f"{(i + 1) / len(df.index):.1%} processed")
+            print(f"creating {label} IDs ... {(i + 1) / len(df):.1%} processed", end="\r")
             time_interval = time_interval + 10
     df["id"] = ids
-    print("complete")
+    print(f"creating {label} IDs ... complete")
     return df
-
+            
+def format_candidates(data):
+    ''' format the candidates dataframe '''
+    data["votes"] = 0
+    data["primary"] = 0
+    data["cname"] = data["cname"].apply(lambda x: x.split(", ")[1] + " " + x.split(", ")[0])
+    data["party"] = data["pcode"].apply(lambda x: active_parties.loc[x].pabbrev)
+    data = create_ids("candidate", data)
+    data.set_index("id", inplace=True)
+    data = data[["cname", "party", "primary", "votes"]]
+    return data            
+            
 # BEGIN PROGRAM
+
+if __name__ == "__main__":
+    print("2020 ACT ELECTION SIMULATOR")
+    print("\u00a9 Markus Mannheim (ABC Canberra)")
     
-print("2020 ACT election simulator — Yerrabi")
+    # read in parameters
+    print("\nestablishing databases ...", end=" ")
+    parties = pd.read_csv("./data/Groups.txt")
+    candidates = pd.read_csv("./data/Candidates.txt")
+    electorates = pd.read_csv("./data/Electorates.txt")
+    print("complete")
 
-# read in data
-print("\nreading in data ...")
-groups = pd.read_csv("./data/Groups.txt")
-candidates = pd.read_csv("./data/Candidates.txt")
-yerrabi = pd.read_csv("./data/YerrabiTotal.txt", usecols=["pindex", "pref", "pcode", "ccode"])
-print("complete")
+    # read in votes
+    print("reading in ballot papers ...", end=" ")
+    ballots = pd.DataFrame(columns=["pindex", "pref", "pcode", "ccode", "ecode"])
+    for electorate in electorates.index:    
+        add_ballots = pd.read_csv("./data/YerrabiTotal.txt", usecols=["pindex", "pref", "pcode", "ccode"])
+        add_ballots["ecode"] = electorates.loc[electorate].ecode
+        ballots = ballots.append(add_ballots)
+    print("complete")
 
-# create temporary vote sample
-# yerrabi = yerrabi[175000:200000]
+    # begin cycle    
+    while True:
+        electorate = get_electorate()
+        # user wants to quit
+        if electorate == 5:
+            break
+        
+        # activate electorate simulation
+        
+        # filter data to chosen electorate
+        print(f"\nfiltering {electorates.loc[electorate].electorate} data ...", end=" ")
+        active_ballots = ballots[ballots["ecode"] == electorate]
+        active_candidates = candidates[candidates["ecode"] == electorate]
+        active_parties = parties[parties["ecode"] == electorate].set_index("pcode")
+        print("complete")
+        
+        # creat sample data to limit speed up calculation
+        print(f"sampling data for analysis...", end=" ")
+        sample_ballots = active_ballots.sample(frac=.1, random_state=2)
+        print("complete")
 
-print("\nfiltering data ...")
+        # format candidate and ballot data
+        active_candidates = format_candidates(active_candidates)
+        print()
+        print(active_candidates)
 
-# limit datasets to Yerrabi-only
-groups = groups[groups["ecode"] == 5].set_index("pcode")
-candidates = candidates[candidates["ecode"] == 5]
-print("complete")
+        # print("\npreparing preference data ...")
 
-print("\ncreating candidate IDs ...")
+        # # assign candidate IDs to each vote preference
+        # yerrabi = create_ids(yerrabi)
+        # yerrabi[["pindex", "pref", "pcode", "id"]]
 
-# create candidate IDs
-candidates["votes"] = [0] * len(candidates)
-candidates["primary"] = [0] * len(candidates)
-candidates["cname"] = candidates["cname"].apply(lambda x: x.split(", ")[1] + " " + x.split(", ")[0])
-candidates["party"] = candidates["pcode"].apply(lambda x: groups.loc[x].pabbrev)
-candidates = create_ids(candidates)
-candidates.set_index("id", inplace=True)
-candidates = candidates[["cname", "party", "primary", "votes"]]
-
-print("\npreparing preference data ...")
-
-# assign candidate IDs to each vote preference
-yerrabi = create_ids(yerrabi)
-yerrabi[["pindex", "pref", "pcode", "id"]]
-
-print("\ncreating voter files ...")
-
-# group vote preferences into individual voters
-voters = pd.DataFrame(index=yerrabi.pindex.unique(), columns=["votes", "pref", "value"])
-start_time = time.time()
-time_interval = 10
-for i, indice in enumerate(voters.index):
-    data = yerrabi[yerrabi["pindex"] == indice].sort_values("pref")
-    votes = []
-    for j in data.index:
-        votes.append(data.loc[j].id)
-    voters.loc[indice].votes = votes
-    voters.loc[indice].pref = 0
-    voters.loc[indice].value = 1
-    if (time.time() - start_time) > time_interval:
-        print(f"{(i + 1) / len(voters.index):.1%} processed")
-        time_interval = time_interval + 10
-print("complete")
-
-quota = len(voters) / 6 + 1
-print(f"\nquota established: {quota:.1f}")
-
-print("\ncreate final lists")
-
-elected_candidates = pd.DataFrame(columns=["cname", "party", "primary"])
-eliminated_candidates = pd.DataFrame(columns=["cname", "party", "primary"])
-exhausted_votes = 0
-print("complete")
-
-print("\ndistribute initial votes ...")
-
-# begin counting rounds
-count_no = 1
-assign_votes()
+        # print("\ncreating voter files ...")
+        
+        
+    
+    # exit program
+    print("\nEnjoy your day.")
+    # exit()
